1f6d27c8dbdc2d906e87c3b815d16bad0ac3f5b8
Luiz Eduardo <euardo8@gmail.com>
2021-06-10 16:33:45 -0300

    Initial commit

---
+++ src/views/partials/header.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>CRUD MVC</title>
+        <link rel="stylesheet" href="<?=$base?>/assets/css/style.css" />
+    </head>
+    <body>
+        <header>
+            <h1>Lista de E-MAILS</h1>
+            <hr>
+        </header>
